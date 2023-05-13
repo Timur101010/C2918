7a0d65ff53f31481c3b19ba9d828c2a1572e5aba
Timur101010 <lisitskiyt@gmail.com>
2023-05-13 16:10:34 +0300

    student_hw

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("test");
-print("hello world!");
+class Dog:
+    def __init__(self, age, gladness, name):
+        self.age = age
+        self.gladness = gladness
+        self.name = name
+
+    def age (self):
+        age  = 1
+
+    def gladness (self):
+        gladness = 100
+
+
+    def name (self, busya=None):
+        name = busya
+
+
+print("name=Busya" /n, "gladness=100" /n, "age=1")
