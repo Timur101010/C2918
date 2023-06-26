e79d0a4e70aac6e5fdd22a4e6eb2c70339efd24f
Timur101010 <lisitskiyt@gmail.com>
2023-06-26 11:18:42 +0300

    binary_search

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-print("test");
-print("hello world!");
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+# Пример использования
+arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+target = 23
+
+result = binary_search(arr, target)
+
+if result != -1:
+    print("Элемент найден на позиции", result)
+else:
+    print("Элемент не найден")
