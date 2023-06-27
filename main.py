57e2f284ce0b78613744e6c699bc75178bd57e41
Timur101010 <lisitskiyt@gmail.com>
2023-06-27 13:00:09 +0300

    interface-into-flat_library

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("test");
-print("hello world!");
+import flet as ft
+import page as page
+
+
+# pip install flet
+def main (page: ft. Page) :
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft. MainAxisAlignment. CENTER
+    
+    txt_number = ft. TextField (value="g", text_align=ft. TextAlign.RIGHT, width=100)
+def minus_click(e, txt_number=None):
+    txt_number .value = strint (txt_number. value) - 1
+    page.update ()
+def plus_click(e, page=None):
+    txt_number.value = str (int (txt_number. value) + 1)
+    page.update ()
+    page.add()
