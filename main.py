b462b565ea19c1e7c4f4549014ae71001b88d2db
Timur101010 <lisitskiyt@gmail.com>
2023-07-01 11:05:43 +0300

    dp-prog-mm_commit

--- main.py
+++ main.py
@@ -1,2 +1,46 @@
-print("test");
-print("hello world!");
+import sys
+from time import strftime, gmtime
+import threading
+from time import sleep
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQml import QQmlApplicationEngine
+from PyQt5.QtCore import QObject, pyqtSi
+
+
+def pyqtSignal(str, arguments):
+    pass
+
+
+class Backend(QObject):
+    updated = pyqtSignal(str, arguments=['updater'])
+
+
+def __init__(self):
+    QObject.__init__(self)
+
+
+def bootUp(self):
+    t_thread = threading.Thread(target=self._bootUp)
+    t_thread.daemon = True
+    t_thread.start()
+
+
+def _bootUp(self):
+    while True:
+        curr_time = strftime("%H:%M:%S", gmtime())
+        print(curr_time)
+        self.updater(curr_time)
+        sleep(1)
+
+
+app = QGuiApplication(sys.argv)
+engine = QQmlApplicationEngine()
+
+backend = Backend()
+engine.rootContext().setContextProperty("backend", backend)
+
+engine.load('./UI/main.qml')
+
+backend.bootUp()
+
+sys.exit(app.exec())
