f7f5d35785fd31135a85ff61c0ae46d0e20be18c
Timur101010 <lisitskiyt@gmail.com>
2023-07-10 12:55:33 +0300

    convert_currency

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("test");
-print("hello world!");
+def convert_currency(amount, rate):
+    return amount * rate
+
+usd_to_eur = 0.85
+amount_in_usd = 100
+amount_in_eur = convert_currency(amount_in_usd, usd_to_eur)
+print(amount_in_eur)
