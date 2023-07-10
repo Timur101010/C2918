7741b7994fd5170614c67c910614558dc500a71c
Timur101010 <lisitskiyt@gmail.com>
2023-07-10 12:57:10 +0300

    celendar

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("test");
-print("hello world!");
+import calendar
+
+year = int(input("Enter the year: "))
+month = int(input("Enter the month: "))
+
+cal = calendar.month(year, month)
+print(cal)
