861130194846227411a05979d692380df3b1aa82
Timur101010 <lisitskiyt@gmail.com>
2023-07-10 12:56:24 +0300

    notes

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("test");
-print("hello world!");
+notes = []
+
+def add_note():
+    note = input("Enter a note: ")
+    notes.append(note)
+
+def view_notes():
+    for note in notes:
+        print(note)
+
+while True:
+    choice = input("Enter 'a' to add a note, 'v' to view notes, or 'q' to quit: ")
+    if choice == 'a':
+        add_note()
+    elif choice == 'v':
+        view_notes()
+    elif choice == 'q':
+        break
+    else:
+        print("Invalid choice. Try again.")
