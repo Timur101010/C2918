2fccd58930c334d7d7a671143124c6e73ba296b9
Timur101010 <lisitskiyt@gmail.com>
2023-07-10 12:53:58 +0300

    calculator

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("test");
-print("hello world!");
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+print(add(5, 3))
+print(subtract(7, 2))
+print(multiply(4, 6))
+print(divide(8, 2))
