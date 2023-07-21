c272ab76b8678eb671199188b392f364785245e1
Timur101010 <lisitskiyt@gmail.com>
2023-07-21 10:56:13 +0300

    click-app

--- main.py
+++ main.py
@@ -1,74 +1,170 @@
+# print_user_agent.py
 import requests
-# import pandas as pd
 
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'
+json = requests.get('http://httpbin.org/user-agent').json()
+print(json['user-agent'])
 
-res = requests.get(url)
-res_dict = res.json()
+import argparse
 
-repos = res_dict ["items"]
-len(repos)
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
 
-import requests 
-import pandas as pd
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'
-res = requests.get (url)
-res_dict = res. json ( )
-repos = res_dict[ 'items']
-len (repos)
-30
+args = parser.parse_args()
+print(args.accumulate(args.integers))
 
-del res_dict['items']
-res_dict
-{'incomplete_results': True,'total count': 8046758}
+# cli.py
+import click
 
-repo_df = pd.DataFrame(repos)
+@click.command()
+def main():
+    print("I'm a beautiful CLI ✨")
 
-repo_df = repo_df[['name', 'full_name', 'html_url', 'created_at', 'stargazers_count', 'watchers', 'forks', 'open_issues']]
-repo_df['created_at'] = pd.to_datetime(repo_df['created_at'])
-repo_df['created_year'] = repo_df['created_at'].dt.year
-repo_df['years_on_github'] = 2022 - repo_df['created_at'].dt.year
+if __name__ == "__main__":
+    main()
 
-repo_df = pd. DataFrame (repos)
-repo_df = repo_df[['name', 'full _name', 'html _url', 'created at', 'stargazers_ count', 'watchers', 'forks', 'open_issues']]
-repo_df['created_at'] = pd.to_datetime(repo_df['created_at'])
-repo_df ['created _year'] = repo_df['created_at'].dt.year
-repo_df['years _on_github'] = 2022 - repo_df['created_at'].dt.year
-repo_df
-
-
-
-# import requests
-# import pandas as pd
-#
-# url = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'
-#
-# res = requests.get(url)
-# res_dict = res.json()
-#
-# repos = res_dict["items"]
-# len(repos)
-#
-# repo_df = pd.DataFrame(repos)
-#
-# repo_df = repo_df[['name', 'full_name', 'html_url', 'created_at', 'stargazers_count', 'watchers', 'forks', 'open_issues']]
-# repo_df['created_at'] = pd.to_datetime(repo_df['created_at'])
-# repo_df['created_year'] = repo_df['created_at'].dt.year
-# repo_df['years_on_github'] = 2022 - repo_df['created_at'].dt.year
-#
-# repo_df
-
-
-
-
-
-
-
-
-
+var = {
+    "base": "stations",
+    "clouds": {
+        "all": 90
+    },
+    "cod": 200,
+    "coord": {
+        "lat": 51.51,
+        "lon": -0.13
+    },
+    "dt": 1485789600,
+    "id": 2643743,
+    "main": {
+        "humidity": 81,
+        "pressure": 1012,
+        "temp": 280.32,
+        "temp_max": 281.15,
+        "temp_min": 279.15
+    },
+    "name": "London",
+    "sys": {
+        "country": "GB",
+        "id": 5091,
+        "message": 0.0103,
+        "sunrise": 1485762037,
+        "sunset": 1485794875,
+        "type": 1
+    },
+    "visibility": 10000,
+    "weather": [
+        {
+            "description": "light intensity drizzle",
+            "icon": "09d",
+            "id": 300,
+            "main": "Drizzle"
+        }
+    ],
+    "wind": {
+        "deg": 80,
+        "speed": 4.1
+    }
+}
 
 
+import requests
 
+SAMPLE_API_KEY = 'b1b15e88fa797225412429c1c50c122a1'
+
+def current_weather(location, api_key=SAMPLE_API_KEY):
+    url = 'http://samples.openweathermap.org/data/2.5/weather'
+
+    query_params = {
+        'q': location,
+        'appid': api_key,
+    }
+
+    response = requests.get(url, params=query_params)
+
+    return response.json()['weather'][0]['description']
+
+def current_weather(location, api_key=SAMPLE_API_KEY):
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+
+    # дальше всё остаётся как было
+    ...
+
+@click.command()
+@click.argument('location')
+@click.option('--api-key', '-a')
+def main(location, api_key):
+    weather = current_weather(location, api_key)
+    print(f"The weather in {location} right now: {weather}.")
+
+@click.command()
+@click.argument('location')
+@click.option(
+    '--api-key', '-a',
+    help='your API key for the OpenWeatherMap API',
+)
+def main(location, api_key):
+    ...
+
+...
+def main(location, api_key):
+    """
+    A little weather tool that shows you the current weather in a LOCATION of
+    your choice. Provide the city name and optionally a two-digit country code.
+    Here are two examples:
+
+    1. London,UK
+
+    2. Canmore
+
+    You need a valid API key from OpenWeatherMap for the tool to work. You can
+    sign up for a free account at https://openweathermap.org/appid.
+    """
+    ...
+
+import click
+import requests
+
+SAMPLE_API_KEY = 'b1b15e88fa797225412429c1c50c122a1'
+
+
+def current_weather(location, api_key=SAMPLE_API_KEY):
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+
+    query_params = {
+        'q': location,
+        'appid': api_key,
+    }
+
+    response = requests.get(url, params=query_params)
+
+    return response.json()['weather'][0]['description']
+
+
+@click.command()
+@click.argument('location')
+@click.option(
+    '--api-key', '-a',
+    help='your API key for the OpenWeatherMap API',
+)
+def main(location, api_key):
+    """
+    A little weather tool that shows you the current weather in a LOCATION of
+    your choice. Provide the city name and optionally a two-digit country code.
+    Here are two examples:
+    1. London,UK
+    2. Canmore
+    You need a valid API key from OpenWeatherMap for the tool to work. You can
+    sign up for a free account at https://openweathermap.org/appid.
+    """
+    weather = current_weather(location, api_key)
+    print(f"The weather in {location} right now: {weather}.")
+
+
+if __name__ == "__main__":
+    main()
 
 
 
